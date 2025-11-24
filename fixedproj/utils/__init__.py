b9091459e3f02be31846fe053be4bed2c7d736b4
Abhishek Jacob Santhosh <abhisheksanthosh45@gmail.com>
2025-11-24 12:47:22 +0530

    Create __init__.py

---
+++ fixedproj/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities module for Research Paper Generator"""
+from .text_processing import TextProcessor
+from .cache_manager import CacheManager
+
+__all__ = ['TextProcessor', 'CacheManager']
