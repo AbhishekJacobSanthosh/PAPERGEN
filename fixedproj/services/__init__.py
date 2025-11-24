59d29a97c3faeae41d55a3958604443c5bc45a6c
Abhishek Jacob Santhosh <abhisheksanthosh45@gmail.com>
2025-11-24 12:43:57 +0530

    Create __init__.py

---
+++ fixedproj/services/__init__.py
@@ -0,0 +1,5 @@
+from .export_service import ExportService
+from .figure_generator import FigureGeneratorService
+from .ocr_service import OCRService
+from .paper_generator import PaperGeneratorService
+from .rag_service import RAGService
